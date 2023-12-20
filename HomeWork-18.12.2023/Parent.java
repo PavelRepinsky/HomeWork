c6ccea47496d321965b00b6c78dbd21fac78e599
Pavel Repinsky <154328944+PavelRepinsky@users.noreply.github.com>
2023-12-20 08:17:45 +0300

    Add files via upload

---
+++ HomeWork-18.12.2023/Parent.java
@@ -0,0 +1,33 @@
+package com.company.Homework18_12_2023;
+
+public abstract class Parent {
+
+    private String name;
+
+    //Статические блоки
+    static {
+        System.out.println("Parent:static 1");
+    }
+
+    static {
+        System.out.println("Parent:static 2");
+    }
+
+    //Нестатические блоки
+    {
+        System.out.println("Parent:instance 1");
+    }
+
+    {
+        System.out.println("Parent:instance 2");
+    }
+
+    //Конструкторы
+    public Parent() {
+        System.out.println("Parent:constructor");
+    }
+
+    public Parent(String name) {
+        System.out.println("Parent:name-constructor");
+    }
+}
